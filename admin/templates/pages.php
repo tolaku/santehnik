8a1477ddd17d5675446c58e45bbff491502747f1
Anatol <tola_k@mail.ru>
2018-01-24 23:00:41 +0300

    leftbar

--- admin/templates/pages.php
+++ admin/templates/pages.php
@@ -1,5 +1,5 @@
 <?php defined('PLUMBER') or die('Закрыт доступ'); ?>
-<?php print_arr($page); ?>
+<?php //print_arr($page); ?>
 		<div class="content">
 			<h2>Список страниц</h2>
 			<a href="#"><img class="add_some" src="<?=TEMPLATE_ADMIN?>images/add_page.jpg" alt="добавить страницу" /></a>
@@ -10,45 +10,16 @@
 				<th class="str_sort">Сортировка</th>
 				<th class="str_action">Действие</th>
 			  </tr>
-
+			  <?php $i=1;  ?>
 			   <?php foreach($page as $key => $item): // выводим разделы?>
 				  <tr>
-					<td>1</td>
+					<td><?=$i?></td>
 					<td class="name_page"><?=$item['name']?></td>
 					<td>1</td>
-					<td><a href="#" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
+					<td><a href="?view=<?=$item[0]?>" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
 				  </tr>
+				  <?php $i++; ?>
 			  <?php endforeach; ?>
-			  <tr>
-				<td>1</td>
-				<td class="name_page">Оплата и доставка</td>
-				<td>1</td>
-				<td><a href="#" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
-			  </tr>
-			  <tr>
-				<td>2</td>
-				<td class="name_page">Оплата и доставка</td>
-				<td>2</td>
-				<td><a href="#" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
-			  </tr>
-			  <tr>
-				<td>3</td>
-				<td class="name_page">Оплата и доставка</td>
-				<td>3</td>
-				<td><a href="#" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
-			  </tr>
-			  <tr>
-				<td>4</td>
-				<td class="name_page">Оплата и доставка</td>
-				<td>4</td>
-				<td><a href="#" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
-			  </tr>
-			  <tr>
-				<td>5</td>
-				<td class="name_page">Оплата и доставка</td>
-				<td>5</td>
-				<td><a href="#" class="edit">изменить</a>&nbsp; | &nbsp;<a href="#" class="del">удалить</a></td>
-			  </tr>
 			</table>
 			<a href="#"><img class="add_some" src="<?=TEMPLATE_ADMIN?>images/add_page.jpg" alt="добавить страницу" /></a>
 
